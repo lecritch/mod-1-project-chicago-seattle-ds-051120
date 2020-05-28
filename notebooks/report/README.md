6c6fe9d7347f1384feee13e909be386d02a3a272
Leana Critchell <leana.critchell@hotmail.com>
2020-05-28 15:47:03 -0700

    notbooks 12 and 13 added with updated education data for oy & non-oy

--- notebooks/report/README.md
+++ notebooks/report/README.md
@@ -1,3 +1,11 @@
 # Report
 
 This directory contains a polished notebook that states the business problem and summarizes your solution, along with high level takeaways.
+
+# part 1 map:
+
+
+# part 2 & 3 analysis 
+
+
+# part 4
--- src/data_cleaning/data_cleaning.py
+++ src/data_cleaning/data_cleaning.py
@@ -17,19 +17,21 @@ def create_kc_df():
     # create df of just WA entries
     puma_names_2010_wa = pd.read_sql("SELECT * FROM puma_names_2010 WHERE state_name = 'Washington';", conn)
 
+    kc_puma_codes = list(map(str, list(range(11601, 11617))))
+    
     # subset just the king county entries
-    king_county = puma_names_2010_wa[puma_names_2010_wa['puma_name'].str.match('^King County*')== True]
+    king_county = puma_names_2010_wa[puma_names_2010_wa['puma'].isin(kc_puma_codes)]
     
     conn.close()
     
     return king_county
 
-def create_skc_df():
+def create_skc_puma_df():
     """
     Function to recreate the skc puma codes df
     """
     # get puma codes and create skc_df
-    skc_puma_codes = ['11613', '11614', '11615', '11612']
+    skc_puma_codes = list(map(str, list(range(11612, 11616))))
     
     # create skc df from king county df:
     skc_pumas = create_kc_df()[create_kc_df()['puma'].isin(skc_puma_codes)]
@@ -44,7 +46,7 @@ def skc_df():
     # Create a connection to db
     conn = psycopg2.connect(dbname=DBNAME)
     
-    skc_puma_codes = ['11613', '11614', '11615', '11612']
+    skc_puma_codes = list(map(str, list(range(11612, 11616))))
     
     QUERY = """
     SELECT serialno AS id, agep AS age, sex, pwgtp AS person_weight, puma, sch AS school_enrollment, schl AS education_attained, 
@@ -84,3 +86,46 @@ def add_cols_skc(df):
     df['is_oy'] = (df['employment_status'].isin(oy_employment_status)) & (df['school_enrollment'].isin(oy_school_enrollment))
     
     return df
+
+def final_skc_df():
+    """
+    A function that creates the final df used to analyse pums data with the added age group and is_oy columns.
+    """
+    # establish df from sql querying 2017 pums:
+    df = skc_df()
+    
+    final_skc_df = add_cols_skc(df)
+    
+    return final_skc_df
+
+
+def pad(num):
+    """
+    Create function to add 0's to single-digit entries:
+    """
+    if num < 10:
+        return f'0{num}'
+    else: 
+        return str(num)  
+    
+def not_oy_dfs(ed_codes):
+    """
+    Function to create df based on education codes of non-oy pop
+    ed_codes (list)
+    """
+    skc_df = final_skc_df()
+    
+    not_oy_df = skc_df[skc_df['is_oy'] == False]
+    
+    codes = ed_codes
+
+    df = not_oy_df[not_oy_df['education_attained'].isin(codes)]
+    
+    return df
+
+def age_breakdown(df):
+    """
+    A function to print out the number of people per age group for a dataframe
+    """
+    for ag in df['age_group'].unique():
+        print(f"{ag}: {df[df['age_group'] == ag]['person_weight'].sum()}")
